496016eefab0562a432491441aab89e57ed3e349
KirtiNa <nawalekirtiwit@gmail.com>
2020-11-06 11:44:14 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore, combineReducers } from 'redux';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import productsReducer from '../reducers/productsReducer';
 import categoriesReducer from '../reducers/categoriesReducer';
-
 const rootReducer = combineReducers({
     products : productsReducer,
     categories : categoriesReducer
@@ -10,7 +10,24 @@ const rootReducer = combineReducers({
 
 //const store = createStore(productsReducer);
 //const store = createStore(categoriesReducer);
+function logger(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.log('Before -> ', store.getState());
+            console.log(action);
+            next(action);
+            console.log("After -> ", store.getState());
+            console.groupEnd();
+        }
+    }
+}
 
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+//important : the following line has to REMOVED when the application is deployed for production
+window['store'] = store;
+
 // store state = { products : [], categories : []}
-export default store;
+export default store; 
